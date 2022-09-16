2cd4f9dc3e098cd8c78c312bca97228333ff1289
sivaecribm <113658479+sivaecribm@users.noreply.github.com>
2022-09-16 16:45:39 +0530

    Update server.tf

--- server.tf
+++ server.tf
@@ -2,14 +2,14 @@ provider "aws" {
 region = "us-east-2"
 }
 resource "aws_instance" "myawsserver" {
-  ami = "ami-077e31c4939f6a2f3"
+  ami = "ami-0568773882d492fc8"
   instance_type = "t2.micro"
-  key_name = "gagan-import"
+  key_name = "SivaAnsibleKeyPair"
 
   tags = {
-    Name = "Gagan-DevOps-server-IBM"
+    Name = "Siva-server-IBM"
     env = "production"
-    owner = "Gagandeep"
+    owner = "Siva"
   }
   provisioner "local-exec" {
     command = "echo The servers IP address is ${self.public_ip} && echo ${self.public_ip} > /tmp/inv"
